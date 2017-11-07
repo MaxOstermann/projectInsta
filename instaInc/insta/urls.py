6243ab467a73232db17b7de51cc11a3d209e9aaf
nik <borecnick@mail.ru>
2017-11-07 21:57:21 +0300

    first3

--- instaInc/insta/urls.py
+++ instaInc/insta/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'insta'
 urlpatterns = [
     url(r'^$', views.reg, name='reg'),
+    url(r'^auth/', views.login, name='login'),
+    url(r'^lout/', views.lout, name='lout'),
+    url(r'^exit/', views.log_out, name='exit'),
     url(r'^uspeh/', views.uspeh, name='ura'),
 ]
--- instaInc/insta/views.py
+++ instaInc/insta/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.http import Http404
 from django.urls import reverse
 from django.views import generic
@@ -17,5 +17,34 @@ def reg(request):
 
     return render(request, 'insta/reg.html', {"form": form})
 
+
 def uspeh(request):
     return render(request, 'insta/uspeh.html')
+
+
+def login(request):
+    if request.method == "POST":
+        try:
+            m = InstaUser.objects.get(nickname_user=request.POST['username'])
+            if m.password == request.POST['password']:
+                request.session['member_id'] = m.id
+                return HttpResponse("Вы авторизованы.")
+            else:
+                return HttpResponse("Неправильный пароль.")
+        except InstaUser.DoesNotExist:
+            return HttpResponse("Ваши логин и пароль не соответствуют.")
+
+    return render(request, 'insta/auth.html')
+
+
+def lout(request):
+    return render(request, 'insta/lout.html')
+
+
+def log_out(request):
+    try:
+        del request.session['member_id']
+    except KeyError:
+        pass
+
+    return HttpResponse("Вы вышли.")
