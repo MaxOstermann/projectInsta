cde6c0a42c97951a4b44c73c7afdf457d0489b90
MaxOstermann <borecnick@mail.ru>
2019-01-31 10:49:01 +0300

    Add readme.md and change root url

--- instaInc/urls.py
+++ instaInc/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 
 
 urlpatterns = [
-    url(r'^insta/', include('insta.urls')),
+    url(r'', include('insta.urls')),
     url(r'^admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
